87f7be144d84b99347b10a8b21bb7caa2cbe57d5
hieudt1107 <57468121+hieudt1107@users.noreply.github.com>
2024-06-21 11:14:22 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -14,15 +14,15 @@ yes | pkg install libjansson
 # Clone repository ccminer
 git clone https://github.com/hieudt1107/ccminer.git
 
+# Thêm lệnh vào .bashrc để chạy ccminer khi Termux khởi động
+echo '~/ccminer/ccminer -c ~/ccminer/config.json' >> .bashrc
+
 # Chuyển đến thư mục ccminer
 cd ccminer
 
 # Cấp quyền thực thi cho các tệp
 chmod +x ccminer start.sh run
 
-# Quay trở lại thư mục chính
-cd
 
-# Thêm lệnh vào .bashrc để chạy ccminer khi Termux khởi động
-echo '~/ccminer/ccminer -c ~/ccminer/config.json' >> .bashrc
+
 
